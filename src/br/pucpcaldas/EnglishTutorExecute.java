6869f4d14c924b77e33c71ec5e0cfe35f8cf62dd
Elton Fonseca <eltonfonseca1994@gmail.com>
2018-10-15 21:23:57 -0300

    Tela de Login

--- src/br/pucpcaldas/EnglishTutorExecute.java
+++ src/br/pucpcaldas/EnglishTutorExecute.java
@@ -5,26 +5,7 @@
  */
 package br.pucpcaldas;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXButton.ButtonType;
-import java.awt.Dimension;
-import java.awt.Toolkit;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Paint;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
--- src/br/pucpcaldas/Login.java
+++ src/br/pucpcaldas/Login.java
@@ -7,15 +7,22 @@ package br.pucpcaldas;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXButton.ButtonType;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 /**
  *
@@ -25,12 +32,17 @@ public class Login {
     
     private static Login login;
     private final Scene scene;
+    private final GridPane grid;
     private final BorderPane borderPane;
-    private final BorderPane borderPaneButtons;
     private final Dimension screen;
     private final JFXButton btnLogin;
     private final JFXButton btnExit;
     private final JFXButton btnCadastrar;
+    private final JFXTextField usuario;
+    private final JFXPasswordField senha;
+    private final Text lbl_usuario;
+    private final Text lbl_senha;
+    private final Text sceneTitle;
     private final int width;
     private final int height;
     
@@ -39,22 +51,42 @@ public class Login {
         
         this.screen = Toolkit.getDefaultToolkit().getScreenSize();
         this.borderPane = new BorderPane();
-        this.borderPaneButtons = new BorderPane();
+        this.grid = new GridPane();
         this.btnLogin = new JFXButton("ENTRAR");
         this.btnExit = new JFXButton("SAIR");
         this.btnCadastrar = new JFXButton("CADASTRAR");
+        this.usuario = new JFXTextField();
+        this.senha = new JFXPasswordField();
+        this.lbl_senha = new Text("Senha: ");
+        this.lbl_usuario = new Text("Usuário: ");
+        this.sceneTitle = new Text("EnglishTutor - Login");
         
         width = (int)(screen.width * 0.40);
         height = (int)(screen.height * 0.60);
+        
+        this.sceneTitle.setFont(Font.font("Arial", FontWeight.BOLD, 30));
+        this.sceneTitle.setFill(Paint.valueOf("#ffffff"));
+        this.lbl_usuario.setFont(Font.font("Arial", 16));
+        this.lbl_usuario.setFill(Paint.valueOf("#ffffff"));
+        this.lbl_senha.setFont(Font.font("Arial", 16));
+        this.lbl_senha.setFill(Paint.valueOf("#ffffff"));
+        
+        this.usuario.setStyle("-fx-text-inner-color: white;");
+        this.usuario.setFont(Font.font("Arial", 14));
+        this.usuario.setPrefSize(300, 30);
+        this.senha.setStyle("-fx-text-inner-color: white;");
+        this.senha.setFont(Font.font("Arial", 14));
+        this.senha.setPrefSize(300, 30);
+  
         this.btnLogin.setFont(Font.font("Arial", FontWeight.BOLD, 12));
-        this.btnLogin.setPrefSize(100, 40);
+        this.btnLogin.setPrefSize(150, 40);
         this.btnLogin.setButtonType(ButtonType.RAISED);
         this.btnLogin.setRipplerFill(Paint.valueOf("#ffffff"));
         this.btnLogin.setTextFill(Paint.valueOf("#ffffff"));
         this.btnLogin.setStyle("-fx-border-radius: 5; -fx-background-color: #095860;");
         
         this.btnExit.setFont(Font.font("Arial", FontWeight.BOLD, 12));
-        this.btnExit.setPrefSize(100, 40);
+        this.btnExit.setPrefSize(150, 40);
         this.btnExit.setButtonType(ButtonType.RAISED);
         this.btnExit.setRipplerFill(Paint.valueOf("#ffffff"));
         this.btnExit.setTextFill(Paint.valueOf("#ffffff"));
@@ -64,19 +96,31 @@ public class Login {
         });
         
         this.btnCadastrar.setFont(Font.font("Arial", FontWeight.BOLD, 12));
-        this.btnCadastrar.setPrefSize(100, 40);
+        this.btnCadastrar.setPrefSize(150, 40);
         this.btnCadastrar.setButtonType(ButtonType.RAISED);
         this.btnCadastrar.setRipplerFill(Paint.valueOf("#ffffff"));
         this.btnCadastrar.setTextFill(Paint.valueOf("#ffffff"));
         this.btnCadastrar.setStyle("-fx-border-radius: 5; -fx-background-color: #1e5799;");
         
-        this.borderPaneButtons.setLeft(this.btnExit);
-        this.borderPaneButtons.setCenter(this.btnLogin);
-        this.borderPaneButtons.setRight(this.btnCadastrar);
-        this.borderPaneButtons.setPadding(new Insets(10, 100, 50, 100));
+        this.grid.setAlignment(Pos.CENTER);
+        this.grid.setHgap(20);
+        this.grid.setVgap(20);
+        this.grid.setPadding(new Insets(10, 10, 10, 10));
+        this.grid.add(this.lbl_usuario, 0, 0, 1, 1);
+        this.grid.add(this.lbl_senha, 0, 1, 1, 1);
+        this.grid.add(this.usuario, 1, 0, 1, 1);
+        this.grid.add(this.senha, 1, 1, 1, 1);
+        this.grid.add(this.btnExit, 1, 4, 1 ,1);
+        this.grid.add(this.btnLogin, 1, 3, 1, 1);
+        this.grid.add(this.btnCadastrar, 1, 2, 1, 1);
         
-        this.borderPane.setBottom(this.borderPaneButtons);
-        this.scene = new Scene(this.borderPane, width, height, Color.OLIVEDRAB);
+        this.borderPane.setAlignment(this.sceneTitle, Pos.TOP_CENTER);
+        this.borderPane.setTop(this.sceneTitle);
+        this.borderPane.setPadding(new Insets(100, 10, 10, 10));
+        this.borderPane.setCenter(this.grid);
+        this.borderPane.setBackground(new Background(new BackgroundFill(Paint.valueOf("#0e4368"), CornerRadii.EMPTY, Insets.EMPTY)));
+        
+        this.scene = new Scene(borderPane, width, height);
     }
     
     public static Login getInstance() {
@@ -85,7 +129,7 @@ public class Login {
         return login;
     }
     
-    public Scene getScene(){
-        return scene;
+    public Scene getScene() {
+        return this.scene;
     }
 }
